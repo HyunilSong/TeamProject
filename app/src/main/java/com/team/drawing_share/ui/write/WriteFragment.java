847b8abe354f7b7001abbe78f00678f736e51c82
sohn1029 <sohn1029@gmail.com>
2020-06-21 19:39:04 +0900

    Make Design
    add 4 activities
    add 3 fragments

---
+++ app/src/main/java/com/team/drawing_share/ui/write/WriteFragment.java
@@ -0,0 +1,35 @@
+package com.team.drawing_share.ui.write;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.team.drawing_share.R;
+
+public class WriteFragment extends Fragment {
+
+    private WriteViewModel writeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        writeViewModel =
+                ViewModelProviders.of(this).get(WriteViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_write, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        writeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
