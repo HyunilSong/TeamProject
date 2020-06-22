3ad65db70ac8cc536eee3af040f5505bbac452e5
sohn1029 <sohn1029@gmail.com>
2020-06-22 17:52:15 +0900

    modify signup activity

--- app/src/main/java/com/team/drawing_share/SignupActivity.java
+++ app/src/main/java/com/team/drawing_share/SignupActivity.java
@@ -1,14 +1,92 @@
 package com.team.drawing_share;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    // 파이어베이스 인증 객체 생성
+    private FirebaseAuth firebaseAuth;
+
+    // 이메일과 비밀번호
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextPasswordCheck;
+
+    private String email = "";
+    private String password = "";
+    private String passwordcheck = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.id_text);
+        editTextPassword = findViewById(R.id.password_text);
+        editTextPasswordCheck = findViewById(R.id.check_text);
     }
+
+    private boolean isValidEmail() {
+        if (email.isEmpty()) {
+            // 이메일 공백
+            System.out.println(email);
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    // 비밀번호 유효성 검사
+    private boolean isValidPasswd() {
+        if (password.isEmpty()) {
+            System.out.println(password);
+            // 비밀번호 공백
+            return false;
+        }
+        else if(!password.equals(passwordcheck)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    private void createUser(String email, String password) {
+        System.out.println("creation start");
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공
+                            Toast.makeText(SignupActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 회원가입 실패
+                            Toast.makeText(SignupActivity.this, R.string.fail_signup, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    public void signUp(View view) {
+        email = editTextEmail.getText().toString();
+        password = editTextPassword.getText().toString();
+        passwordcheck = editTextPasswordCheck.getText().toString();
+
+        if(isValidEmail() && isValidPasswd()) {
+            createUser(email, password);
+        }
+    }
+
 }
