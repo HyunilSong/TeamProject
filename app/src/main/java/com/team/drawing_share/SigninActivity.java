de8d86026f5e62fb259bee6cd081609808785758
sohn1029 <sohn1029@gmail.com>
2020-06-22 16:40:05 +0900

    signin design modification

--- app/src/main/java/com/team/drawing_share/SigninActivity.java
+++ app/src/main/java/com/team/drawing_share/SigninActivity.java
@@ -2,7 +2,9 @@ package com.team.drawing_share;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+    public void go_signup(View view) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
