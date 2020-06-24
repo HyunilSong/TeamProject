d7ede43ec1fbb3dd1b35bf398fd41026d478daab
sohn1029 <sohn1029@gmail.com>
2020-06-25 02:14:36 +0900

    easy login

--- app/src/main/java/com/team/drawing_share/SigninActivity.java
+++ app/src/main/java/com/team/drawing_share/SigninActivity.java
@@ -147,13 +147,13 @@ public class SigninActivity extends AppCompatActivity {
                     }
                 });
     }
-    @Override
-    protected void onDestroy() {
-
-        if(firebaseAuth.getCurrentUser() != null) {
-            firebaseAuth.signOut();
-        }
-        super.onDestroy();
-    }
+//    @Override
+//    protected void onDestroy() {
+//
+//        if(firebaseAuth.getCurrentUser() != null) {
+//            firebaseAuth.signOut();
+//        }
+//        super.onDestroy();
+//    }
 
 }
